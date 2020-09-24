995c0fb28450b1466714ad6868f5f7a3f5d8fbaa
Bogdan Andrushchenko <bandrushchenko15@gmail.com>
2020-09-24 19:21:56 +0300

    corect  task with class ES6

--- src/js/change_color_class.js
+++ src/js/change_color_class.js
@@ -1,43 +1,37 @@
-import { colors, refs, randomIntegerFromInterval } from './source';
+import {
+  colors,
+  refs,
+  randomIntegerFromInterval
+} from './source';
 
 class Switch_color {
-    constructor({
-        intervalId,
-        isActivBtnStart,
-        time,
-
-    }) {
-
-        this.intervalId = intervalId;
-        this.isActivBtnStart = isActivBtnStart;
-        this.time = time;
-    }
-    changeColor() {
-        const index = randomIntegerFromInterval( 0, colors.length - 1);
-        refs.body.style.backgroundColor = colors[index];
-    }
-    start() {
-        this.isActivBtnStart = true;
-        refs.start.setAttribute("disabled", "disabled")
-        this.intervalId = setInterval(this.changeColor, this.time);
-
-    }
-    stop() {
-        clearInterval(this.intervalId);
-        refs.start.removeAttribute("disabled")
-        this.intervalId = null;
-        this.isActivBtnStart = false;
-        refs.body.style.backgroundColor = '#fff';
-    }
-
+  constructor(intervalId) {
+    this.intervalId = intervalId;
+  }
+  _changeColor() {
+    const index = randomIntegerFromInterval(0, colors.length - 1);
+    refs.body.style.backgroundColor = colors[index];
+  }
+  start() {
+    this.isActivBtnStart = true;
+    refs.stop.removeAttribute('disabled');
+    refs.start.setAttribute('disabled', '');
+    this.intervalId = setInterval(this._changeColor, 1000);
+  }
+  stop() {
+    clearInterval(this.intervalId);
+    refs.start.removeAttribute('disabled');
+    refs.stop.setAttribute('disabled', '');
+    this.intervalId = null;
+    refs.body.removeAttribute('style');
+  }
 }
-const changeColorBody = new Switch_color( {
-    intervalId: null,
-    isActivBtnStart: false,
-    time: 1000,
-})
 
+const changeColorBody = new Switch_color();
 
+const clickStart = () => changeColorBody.start()
+const clickStop = () => changeColorBody.stop()
 
-refs.start.addEventListener('click', changeColorBody.start.bind(changeColorBody));
-refs.stop.addEventListener('click', changeColorBody.stop.bind(changeColorBody));
+refs.start.addEventListener('click', clickStart);
+
+refs.stop.addEventListener('click', clickStop);
